76ec050e688cb39764b78134e78a3b87b8489da4
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-05-17 00:57:07 -0700

    feat: add methods to retrieve arrival/departure Airports from Flights

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,12 @@
 class Flight < ApplicationRecord
   belongs_to :origin, class_name: 'Airport'
   belongs_to :destination, class_name: 'Airport'
+
+  def departure_airport
+    Airport.find_sole_by id: origin_id
+  end
+
+  def arrival_airport
+    Airport.find_sole_by id: destination_id
+  end
 end
