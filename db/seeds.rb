b3c69e943755f30fa0ccbd3a79d3495c7a8b2de9
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-05-18 17:28:13 -0700

    feat: add Flights to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Flight.delete_all
 Airport.delete_all
 
 Airport.create(
@@ -20,5 +21,29 @@ Airport.create(
   }, {
     name: 'Rome',
     code: 'FCO'
+  }]
+)
+
+Flight.create(
+  [{
+    departure_time: Time.new(2022, 6, 1, 12, 0, 0, '-07:00'),
+    arrival_time: Time.new(2022, 6, 1, 13, 0, 0, '-08:00'),
+    origin_id: 1,
+    destination_id: 2
+  }, {
+    departure_time: Time.new(2022, 6, 1, 14, 0, 0, '-07:00'),
+    arrival_time: Time.new(2022, 6, 1, 15, 0, 0, '-08:00'),
+    origin_id: 2,
+    destination_id: 3
+  }, {
+    departure_time: Time.new(2022, 6, 1, 16, 0, 0, '-07:00'),
+    arrival_time: Time.new(2022, 6, 1, 17, 0, 0, '-08:00'),
+    origin_id: 3,
+    destination_id: 4
+  }, {
+    departure_time: Time.new(2022, 6, 1, 18, 0, 0, '-07:00'),
+    arrival_time: Time.new(2022, 6, 1, 19, 0, 0, '-08:00'),
+    origin_id: 4,
+    destination_id: 2
   }]
 )
