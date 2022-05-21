cac0c5036882e0c085dc431dfc1b39a4d20d7649
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-05-21 14:30:19 -0700

    feat: add Flight method to return formatted departure dates/times

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,6 +2,10 @@ class Flight < ApplicationRecord
   belongs_to :origin, class_name: 'Airport'
   belongs_to :destination, class_name: 'Airport'
 
+  def departures_formatted
+    "#{Flight.first.departure_date.strftime('%m/%d/%y')} - #{departure_time.strftime('%l:%M%p')}"
+  end
+
   def departure_airport
     Airport.find_sole_by id: origin_id
   end
