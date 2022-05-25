2ed3b3b08218881ed41d0ad92ecdcec9b3ea43d8
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-05-24 18:01:39 -0700

    feat: whitelist Flight parameters when searching for available flights

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -12,4 +12,10 @@ class FlightsController < ApplicationController
       )
     end
   end
+
+  private
+
+  def flight_params
+    params.require(:flight).permit(:origin_id, :destination_id,:departure_date, :arrival_time, :available_seats)
+  end
 end
