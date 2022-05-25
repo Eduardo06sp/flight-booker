e721c7d26067a1b189beafc3acff5688f810cf2a
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-05-24 20:29:44 -0700

    refactor: use form_with instead of soft-depracated form_for

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,13 +2,13 @@ class FlightsController < ApplicationController
   def index
     @flights = Flight.all
 
-    if params[:commit]
+    if params
       @selected_flights = Flight.where(
-        origin_id: params[:flight][:origin_id],
-        destination_id: params[:flight][:destination_id],
-        departure_date: params[:flight][:departure_date]
+        origin_id: params[:origin_id],
+        destination_id: params[:destination_id],
+        departure_date: params[:departure_date]
       ).where(
-        'available_seats >= ?', params[:flight][:available_seats]
+        'available_seats >= ?', params[:available_seats]
       )
     end
   end
