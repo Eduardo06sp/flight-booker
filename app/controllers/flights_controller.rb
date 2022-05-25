868204c281118c552fa218f7cb2633d136b77755
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-05-24 18:01:07 -0700

    feat: save Flights filtered by user-specified constraints

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,15 @@
 class FlightsController < ApplicationController
   def index
     @flights = Flight.all
+
+    if params[:commit]
+      @selected_flights = Flight.where(
+        origin_id: params[:flight][:origin_id],
+        destination_id: params[:flight][:destination_id],
+        departure_date: params[:flight][:departure_date]
+      ).where(
+        'available_seats >= ?', params[:flight][:available_seats]
+      )
+    end
   end
 end
