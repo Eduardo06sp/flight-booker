40acfd7a18928434e4f2c923194109dc5a5406c9
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-06-01 00:18:28 -0700

    feat: whitelist flight_id and nested Passenger parameters in BookingsController

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -11,4 +11,8 @@ class BookingsController < ApplicationController
 
   def create
   end
+
+  def passenger_params
+    params.require(:booking).permit(:flight_id, passengers_attributes: [:first_name, :last_name, :email, :phone_number])
+  end
 end
