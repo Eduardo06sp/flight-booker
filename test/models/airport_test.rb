04128e3c7805f17b98c90fb5bb943e64d01f69e0
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-06-06 15:34:19 -0700

    test: verify departing Flights association to Guadalajara airport

--- test/models/airport_test.rb
+++ test/models/airport_test.rb
@@ -1,7 +1,9 @@
 require "test_helper"
 
 class AirportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should associate departing flights to Guadalajara airport' do
+    guadalajara_airport = airports(:guadalajaraairport)
+    guadalajara_departs = guadalajara_airport.departing_flights
+    assert_equal 1, guadalajara_departs.count
+  end
 end
