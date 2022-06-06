941a52d1e3b16015739706659a15ed8e6fbaae0d
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-06-05 23:12:41 -0700

    test: add FlightsController#index route test

--- test/controllers/flights_controller_test.rb
+++ test/controllers/flights_controller_test.rb
@@ -1,8 +1,7 @@
 require "test_helper"
 
 class FlightsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get flights_index_url
-    assert_response :success
+  test 'should route to Flights index' do
+    assert_routing '/flights', controller: 'flights', action: 'index'
   end
 end
