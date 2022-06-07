05daab21e07f025d2a2f1eb08c71afc37738e06f
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-06-06 22:24:17 -0700

    test: verify belongs_to origin association for first flight

--- test/models/flight_test.rb
+++ test/models/flight_test.rb
@@ -1,7 +1,9 @@
 require "test_helper"
 
 class FlightTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should associate Los Angeles airport as origin for first flight' do
+    first_flight = flights(:firstflight)
+    origin = first_flight.origin
+    assert_equal 'LAX', origin.code
+  end
 end
