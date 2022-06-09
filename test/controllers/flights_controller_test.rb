311e668d90f629e756aabe009a67f359d966778e
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-06-08 21:11:52 -0700

    test: display third flight with proper parameters

--- test/controllers/flights_controller_test.rb
+++ test/controllers/flights_controller_test.rb
@@ -4,4 +4,18 @@ class FlightsControllerTest < ActionDispatch::IntegrationTest
   test 'should route to Flights index' do
     assert_routing '/flights', controller: 'flights', action: 'index'
   end
+
+  test 'should display thirdflight' do
+    get flights_path, params: {
+      origin_id: airports(:guadalajaraairport).id,
+      destination_id: airports(:riodejaneiroairport).id,
+      departure_date: '2022-06-01',
+      passengers: '3',
+      commit: 'Search Flights'
+    }
+    assert_response :success
+
+    assert_select '.departure_result_info p:nth-child(2)', text: 'GDL', count: 1
+    assert_select '.arrival_result_info p:nth-child(2)', text: 'SDU', count: 1
+  end
 end
