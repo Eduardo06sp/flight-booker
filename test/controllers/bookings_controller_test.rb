aab62cbe503b5b8b1861e67e27ed900f3f40c210
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-06-09 23:45:07 -0700

    test: verify proper flight result when viewing new Booking with selected flight

--- test/controllers/bookings_controller_test.rb
+++ test/controllers/bookings_controller_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class BookingsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should display new bookings view with selected flight' do
+    get bookings_new_path, params: {
+      flight_id: flights(:fourthflight).id,
+      passengers: '2',
+      commit: 'Save '
+    }
+    assert_response :success
+
+    assert_select '.departure_result_info p:nth-child(2)', text: 'SDU', count: 1
+    assert_select '.arrival_result_info p:nth-child(2)', text: 'LAX', count: 1
+  end
 end
