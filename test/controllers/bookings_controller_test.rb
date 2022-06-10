996d38366d0591d5ba369de6c77504bb26cd2492
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-06-09 23:54:33 -0700

    test: verify presence of 8 inputs if 2 passengers are specified for new Booking

--- test/controllers/bookings_controller_test.rb
+++ test/controllers/bookings_controller_test.rb
@@ -12,4 +12,15 @@ class BookingsControllerTest < ActionDispatch::IntegrationTest
     assert_select '.departure_result_info p:nth-child(2)', text: 'SDU', count: 1
     assert_select '.arrival_result_info p:nth-child(2)', text: 'LAX', count: 1
   end
+
+  test 'should display 8 passenger inputs for 2 passengers' do
+    get bookings_new_path, params: {
+      flight_id: flights(:fourthflight).id,
+      passengers: '2',
+      commit: 'Save '
+    }
+    assert_response :success
+
+    assert_select 'label + input', 8
+  end
 end
