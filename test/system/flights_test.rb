2bbca348eb0a9eb418ddaaf5cc2f5836bbde0f55
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-06-10 21:48:48 -0700

    test: verify proper redirect after selecting a flight via system test

--- test/system/flights_test.rb
+++ test/system/flights_test.rb
@@ -28,4 +28,17 @@ class FlightsTest < ApplicationSystemTestCase
     assert_text '09:00pm', count: 1
     assert_text '10:00pm', count: 1
   end
+
+  test 'selecting flight result redirects to new Booking path' do
+
+    origin_airport = airports(:losangelesairport).id
+    destination_airport = airports(:guadalajaraairport).id
+    secondflight = flights(:secondflight).id
+    visit "/?origin_id=#{origin_airport}&destination_id=#{destination_airport}&passengers=2&departure_date=2022-06-01&commit=Search+Flights"
+
+    choose("flight_id_#{secondflight}")
+    click_on('Save')
+
+    assert_current_path '/bookings/new', ignore_query: true
+  end
 end
