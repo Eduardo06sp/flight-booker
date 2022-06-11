4bb5e9c1ae08b63d0e668ca094572e31683e6a91
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-06-11 13:09:00 -0700

    test: verify route to PassengersController#create

--- test/controllers/passengers_controller_test.rb
+++ test/controllers/passengers_controller_test.rb
@@ -1,4 +1,7 @@
 require "test_helper"
 
 class PassengersControllerTest < ActionDispatch::IntegrationTest
+  test 'should route to Passengers create controller' do
+    assert_routing({ path: '/passengers', method: 'post' }, { controller: 'passengers', action: 'create' })
+  end
 end
