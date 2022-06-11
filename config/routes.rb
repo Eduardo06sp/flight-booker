cfe5f37fb62c4b52aa7d09b1eae40e1df56eae87
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-06-11 13:11:57 -0700

    refactor: use resource routing for PassengersController#create

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   root 'flights#index'
 
   resources :bookings, only: %w[new show create]
-
-  post 'passengers', to: 'passengers#create'
-
   resources :flights, only: %w[index]
+  resources :passengers, only: %w[create]
 end
