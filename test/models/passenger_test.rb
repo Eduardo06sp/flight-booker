a0bb97b603bc7977278c5a8e8cde3c2cdbb5fa72
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-06-12 15:44:22 -0700

    test: verify validity of Passenger with valid attributes

--- test/models/passenger_test.rb
+++ test/models/passenger_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class PassengerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should validate Passenger with no errors' do
+    passenger_info = {
+      first_name: 'Johnny',
+      last_name: 'Bravo',
+      email: 'johnny@bra.vo',
+      phone_number: '4311000000',
+    }
+    passenger = Passenger.new(passenger_info)
+
+    assert passenger.valid?
+  end
 end
