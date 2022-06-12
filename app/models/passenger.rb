f3b1bd2a5702dabee42ec7fca8d223e609585b8e
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-06-12 16:14:36 -0700

    feat: validate presence of Passenger phone_number

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -4,4 +4,5 @@ class Passenger < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
+  validates :phone_number, presence: true
 end
