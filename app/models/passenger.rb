f540e680da01d136a4fc7922cd686f0455789c11
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-06-12 16:57:38 -0700

    feat: validate Passenger email format

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -3,6 +3,6 @@ class Passenger < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :phone_number, presence: true
 end
