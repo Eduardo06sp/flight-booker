af82784768342d6f77f779ef1bc29dcf47a87de2
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-06-12 17:22:36 -0700

    feat: validate Passenger first_name format

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,7 +1,10 @@
 class Passenger < ApplicationRecord
   has_many :bookings
 
-  validates :first_name, presence: true, length: { maximum: 100 }
+  validates :first_name,
+            presence: true,
+            length: { maximum: 100 },
+            format: { without: /[^A-Za-z ]+/ }
   validates :last_name, presence: true
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :phone_number, presence: true
