e649cac1d4882739e5e4561310478d1437cec3b3
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-06-12 17:11:34 -0700

    feat: limit maximum Passenger first_name length to 100 characters

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,7 +1,7 @@
 class Passenger < ApplicationRecord
   has_many :bookings
 
-  validates :first_name, presence: true
+  validates :first_name, presence: true, length: { maximum: 100 }
   validates :last_name, presence: true
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :phone_number, presence: true
