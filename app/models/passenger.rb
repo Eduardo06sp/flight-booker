40b329ba4b64fe28dd779514c502adea8d2080ae
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-06-12 17:25:20 -0700

    feat: validate length of Passenger last_name

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -5,7 +5,9 @@ class Passenger < ApplicationRecord
             presence: true,
             length: { maximum: 100 },
             format: { without: /[^A-Za-z ]+/ }
-  validates :last_name, presence: true
+  validates :last_name,
+            presence: true,
+            length: { maximum: 100 }
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :phone_number, presence: true
 end
