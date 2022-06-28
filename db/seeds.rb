2abdec49cf267bb778371c59802d78a24c81311e
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-06-27 23:35:44 -0700

    feat: update departure/arrival years in Flights seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,118 +26,118 @@ Airport.create(
 
 Flight.create(
   [{
-    departure_date: Date.new(2022, 6, 1),
-    departure_time: Time.new(2022, 6, 1, 12, 0, 0, '-07:00'),
-    arrival_date: Date.new(2022, 6, 1),
-    arrival_time: Time.new(2022, 6, 1, 15, 0, 0, '-05:00'),
+    departure_date: Date.new(2024, 6, 1),
+    departure_time: Time.new(2024, 6, 1, 12, 0, 0, '-07:00'),
+    arrival_date: Date.new(2024, 6, 1),
+    arrival_time: Time.new(2024, 6, 1, 15, 0, 0, '-05:00'),
     available_seats: 20,
 
     origin_id: 1,
     destination_id: 2
   }, {
-    departure_date: Date.new(2022, 6, 1),
-    departure_time: Time.new(2022, 6, 1, 14, 0, 0, '-07:00'),
-    arrival_date: Date.new(2022, 6, 1),
-    arrival_time: Time.new(2022, 6, 1, 17, 0, 0, '-05:00'),
+    departure_date: Date.new(2024, 6, 1),
+    departure_time: Time.new(2024, 6, 1, 14, 0, 0, '-07:00'),
+    arrival_date: Date.new(2024, 6, 1),
+    arrival_time: Time.new(2024, 6, 1, 17, 0, 0, '-05:00'),
     available_seats: 200,
 
     origin_id: 1,
     destination_id: 2
   }, {
-    departure_date: Date.new(2022, 6, 1),
-    departure_time: Time.new(2022, 6, 1, 15, 0, 0, '-07:00'),
-    arrival_date: Date.new(2022, 6, 1),
-    arrival_time: Time.new(2022, 6, 1, 18, 0, 0, '-05:00'),
+    departure_date: Date.new(2024, 6, 1),
+    departure_time: Time.new(2024, 6, 1, 15, 0, 0, '-07:00'),
+    arrival_date: Date.new(2024, 6, 1),
+    arrival_time: Time.new(2024, 6, 1, 18, 0, 0, '-05:00'),
     available_seats: 3,
 
     origin_id: 1,
     destination_id: 2
   }, {
-    departure_date: Date.new(2022, 6, 1),
-    departure_time: Time.new(2022, 6, 1, 19, 0, 0, '-07:00'),
-    arrival_date: Date.new(2022, 6, 1),
-    arrival_time: Time.new(2022, 6, 1, 22, 0, 0, '-05:00'),
+    departure_date: Date.new(2024, 6, 1),
+    departure_time: Time.new(2024, 6, 1, 19, 0, 0, '-07:00'),
+    arrival_date: Date.new(2024, 6, 1),
+    arrival_time: Time.new(2024, 6, 1, 22, 0, 0, '-05:00'),
     available_seats: 1,
 
     origin_id: 1,
     destination_id: 2
   }, {
-    departure_date: Date.new(2022, 6, 1),
-    departure_time: Time.new(2022, 6, 1, 20, 0, 0, '-07:00'),
-    arrival_date: Date.new(2022, 6, 1),
-    arrival_time: Time.new(2022, 6, 1, 23, 0, 0, '-05:00'),
+    departure_date: Date.new(2024, 6, 1),
+    departure_time: Time.new(2024, 6, 1, 20, 0, 0, '-07:00'),
+    arrival_date: Date.new(2024, 6, 1),
+    arrival_time: Time.new(2024, 6, 1, 23, 0, 0, '-05:00'),
     available_seats: 1,
 
     origin_id: 1,
     destination_id: 2
   }, {
-    departure_date: Date.new(2022, 6, 1),
-    departure_time: Time.new(2022, 6, 1, 0, 0, 0, '-07:00'),
-    arrival_date: Date.new(2022, 6, 1),
-    arrival_time: Time.new(2022, 6, 1, 3, 0, 0, '-05:00'),
+    departure_date: Date.new(2024, 6, 1),
+    departure_time: Time.new(2024, 6, 1, 0, 0, 0, '-07:00'),
+    arrival_date: Date.new(2024, 6, 1),
+    arrival_time: Time.new(2024, 6, 1, 3, 0, 0, '-05:00'),
     available_seats: 1,
 
     origin_id: 1,
     destination_id: 2
   }, {
-    departure_date: Date.new(2022, 6, 1),
-    departure_time: Time.new(2022, 6, 1, 2, 0, 0, '-07:00'),
-    arrival_date: Date.new(2022, 6, 1),
-    arrival_time: Time.new(2022, 6, 1, 5, 0, 0, '-05:00'),
+    departure_date: Date.new(2024, 6, 1),
+    departure_time: Time.new(2024, 6, 1, 2, 0, 0, '-07:00'),
+    arrival_date: Date.new(2024, 6, 1),
+    arrival_time: Time.new(2024, 6, 1, 5, 0, 0, '-05:00'),
     available_seats: 1,
 
     origin_id: 1,
     destination_id: 2
   }, {
-    departure_date: Date.new(2022, 6, 1),
-    departure_time: Time.new(2022, 6, 1, 4, 0, 0, '-07:00'),
-    arrival_date: Date.new(2022, 6, 1),
-    arrival_time: Time.new(2022, 6, 1, 7, 0, 0, '-05:00'),
+    departure_date: Date.new(2024, 6, 1),
+    departure_time: Time.new(2024, 6, 1, 4, 0, 0, '-07:00'),
+    arrival_date: Date.new(2024, 6, 1),
+    arrival_time: Time.new(2024, 6, 1, 7, 0, 0, '-05:00'),
     available_seats: 1,
 
     origin_id: 1,
     destination_id: 2
   }, {
-    departure_date: Date.new(2022, 6, 5),
-    departure_time: Time.new(2022, 6, 1, 7, 0, 0, '-07:00'),
-    arrival_date: Date.new(2022, 6, 5),
-    arrival_time: Time.new(2022, 6, 1, 10, 0, 0, '-05:00'),
+    departure_date: Date.new(2024, 6, 5),
+    departure_time: Time.new(2024, 6, 1, 7, 0, 0, '-07:00'),
+    arrival_date: Date.new(2024, 6, 5),
+    arrival_time: Time.new(2024, 6, 1, 10, 0, 0, '-05:00'),
     available_seats: 200,
 
     origin_id: 1,
     destination_id: 2
   }, {
-    departure_date: Date.new(2022, 6, 5),
-    departure_time: Time.new(2022, 6, 1, 11, 0, 0, '-07:00'),
-    arrival_date: Date.new(2022, 6, 5),
-    arrival_time: Time.new(2022, 6, 1, 14, 0, 0, '-05:00'),
+    departure_date: Date.new(2024, 6, 5),
+    departure_time: Time.new(2024, 6, 1, 11, 0, 0, '-07:00'),
+    arrival_date: Date.new(2024, 6, 5),
+    arrival_time: Time.new(2024, 6, 1, 14, 0, 0, '-05:00'),
     available_seats: 200,
 
     origin_id: 1,
     destination_id: 2
   }, {
-    departure_date: Date.new(2022, 6, 1),
-    departure_time: Time.new(2022, 6, 1, 14, 0, 0, '-05:00'),
-    arrival_date: Date.new(2022, 6, 1),
-    arrival_time: Time.new(2022, 6, 1, 17, 0, 0, '-03:00'),
+    departure_date: Date.new(2024, 6, 1),
+    departure_time: Time.new(2024, 6, 1, 14, 0, 0, '-05:00'),
+    arrival_date: Date.new(2024, 6, 1),
+    arrival_time: Time.new(2024, 6, 1, 17, 0, 0, '-03:00'),
     available_seats: 30,
 
     origin_id: 2,
     destination_id: 3
   }, {
-    departure_date: Date.new(2022, 6, 1),
-    departure_time: Time.new(2022, 6, 1, 16, 0, 0, '-03:00'),
-    arrival_date: Date.new(2022, 6, 1),
-    arrival_time: Time.new(2022, 6, 1, 22, 0, 0, '+02:00'),
+    departure_date: Date.new(2024, 6, 1),
+    departure_time: Time.new(2024, 6, 1, 16, 0, 0, '-03:00'),
+    arrival_date: Date.new(2024, 6, 1),
+    arrival_time: Time.new(2024, 6, 1, 22, 0, 0, '+02:00'),
     available_seats: 2,
 
     origin_id: 3,
     destination_id: 4
   }, {
-    departure_date: Date.new(2022, 6, 1),
-    departure_time: Time.new(2022, 6, 1, 18, 0, 0, '+02:00'),
-    arrival_date: Date.new(2022, 6, 1),
-    arrival_time: Time.new(2022, 6, 1, 12, 0, 0, '-05:00'),
+    departure_date: Date.new(2024, 6, 1),
+    departure_time: Time.new(2024, 6, 1, 18, 0, 0, '+02:00'),
+    arrival_date: Date.new(2024, 6, 1),
+    arrival_time: Time.new(2024, 6, 1, 12, 0, 0, '-05:00'),
     available_seats: 1,
 
     origin_id: 4,
