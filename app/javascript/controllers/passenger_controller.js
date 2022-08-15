4af6aecf3bd3bda8cc55f77ba2dec7cbdc491ddc
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-08-15 15:54:57 -0700

    feat: add Stimulus target to Passenger form

--- app/javascript/controllers/passenger_controller.js
+++ app/javascript/controllers/passenger_controller.js
@@ -2,6 +2,8 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="passenger"
 export default class extends Controller {
+  static targets = ['passengerForm'];
+
   connect() {
   }
 }
