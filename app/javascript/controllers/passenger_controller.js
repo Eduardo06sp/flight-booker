97539eb6972f96fa08b631d9151d3b2da058b114
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-08-15 22:54:53 -0700

    feat: add Stimulus target to button that adds new Passenger

--- app/javascript/controllers/passenger_controller.js
+++ app/javascript/controllers/passenger_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="passenger"
 export default class extends Controller {
-  static targets = ['passengerForm'];
+  static targets = ['passengerForm', 'addPassengerButton'];
 
   connect() {
   }
