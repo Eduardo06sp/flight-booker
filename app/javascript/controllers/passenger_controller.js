769397a9c8af2b50492d8d22d8f18e7d4b2764b2
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-08-15 23:32:23 -0700

    feat: create unique Passenger fields in passenger#addPassengerFields

--- app/javascript/controllers/passenger_controller.js
+++ app/javascript/controllers/passenger_controller.js
@@ -6,4 +6,22 @@ export default class extends Controller {
 
   connect() {
   }
+
+  addPassengerFields() {
+    const passengerForm = this.passengerFormTarget.content.cloneNode(true);
+    const passengerLabels = passengerForm.querySelectorAll('label');
+    const passengerInputs = passengerForm.querySelectorAll('input');
+    const currentTime = Date.now();
+
+    const generateUniqueValue = function(element, attribute) {
+      const currentAttributeValue = element.getAttribute(attribute);
+      return currentAttributeValue.replace('0', currentTime);
+    };
+
+    passengerLabels.forEach((label) => label.setAttribute('for', generateUniqueValue(label, 'for')));
+    passengerInputs.forEach((input) => input.setAttribute('id', generateUniqueValue(input, 'id')));
+    passengerInputs.forEach((input) => input.setAttribute('name', generateUniqueValue(input, 'name')));
+
+    this.addPassengerButtonTarget.before(passengerForm);
+  }
 }
