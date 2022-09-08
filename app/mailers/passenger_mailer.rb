46ab647d9832799cc3a55a15fecbab2859ffffbb
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-09-07 23:01:01 -0700

    feat: add confirmation_mail action to PassengerMailer

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,9 @@
 class PassengerMailer < ApplicationMailer
+  default from: 'flight@booker.com'
+
+  def confirmation_email
+    @selected_flight = params[:selected_flight]
+    @passenger = params[:passenger]
+    mail(to: @passenger.email, subject: 'Flight Confirmation')
+  end
 end
